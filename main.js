41acf6fe0d2f522706e232a1ea6e92283de4b3c8
LiliyaShvets <lilianashv@gmail.com>
2020-04-22 22:03:04 +0300

    hw_1 done

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let money = prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    moneyData: money, 
+    timeData: time,
+    expenses: {
+        "expenseItem" : "expenseAmount"
+    },
+    optionalExpenses: {},
+    income: [],
+    saving: false
+};
+ 
+
+let expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
+    expenseAmount = prompt("Во сколько обойдется");
+    expenses = {};
+    expenses[expenseItem] = expenseAmount;
+    console.log(expenses);
+ 
+let dailyExpenseAmount = money / 30;
+alert(dailyExpenseAmount);
